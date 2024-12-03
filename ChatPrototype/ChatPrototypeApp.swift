5e0aa39cabf4baca087e2935fab1a634d4c5520a
Russell Gordon <rgordon@lcs.on.ca>
2024-12-03 08:45:04 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Russell Gordon on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
